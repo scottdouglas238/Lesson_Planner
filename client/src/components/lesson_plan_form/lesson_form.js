88bb4c9eeb540bb1a62cf82b0621005690f62234
RazzleJazzled <robertjgassner@gmail.com>
2020-10-11 16:00:36 -0700

    init commit

---
+++ client/src/components/lesson_plan_form/lesson_form.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function lessonForm() {
+    reutrn (
+        //This will be a Bulma Form
+        //Teacher Name
+
+        //Lesson Name
+
+        //Lesson Department (ie Music or Science)
+
+        //Class (ie 6th Grade math or English 101)
+
+        //Grade Level
+
+        //Concepts
+
+        //Standards
+
+        //Skills
+
+        //Objectives
+
+        //Materials & Resources
+
+        //The above is the "boiler plate" of a lesson plan that will help whoever reads the plan identify what age group/class the lesson is for and what they are to be learning. The below is the procedures for the lesson. 
+
+        //Goal
+
+        //Opening Activity
+
+        //Activity
+
+        //Assessment
+
+        //Closure
+
+        //Some of these could be pretty long. A high school with a block schedule (ie 90 minute classes) will have a lot of activity, and maybe even multiple activities. On the other hand, an elementary classroom may have short lessons to help keep youngsters focused and engaged. Therefore, this form just shoots for the middle with a basic, default plan. Later on in the process, we would work with the client (school district) to formate the lesson plan exactly how they want ti. For now, however, we are just going for a basic layout. 
+
+
+    )
+}
+
+export default lessonForm;
