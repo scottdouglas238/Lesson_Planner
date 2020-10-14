7fa39eb79a352a6c3b80909987fdd55fbd927d0b
RazzleJazzled <robertjgassner@gmail.com>
2020-10-14 11:13:21 -0700

    build jsx for lesson plan

---
+++ client/src/components/display/box.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Box({ children }) {
+    return (
+        <div class="box">
+            {children}
+        </div>
+    )
+} export default Box;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+
 export default {
 newLessonPlan: function(lessonData) {
     return axios.post("/api/lessons", lessonData);
+},
+
+getLesson: function(id) {
+    return axios.get("/api/lesson" + id)
 }
 
 };
