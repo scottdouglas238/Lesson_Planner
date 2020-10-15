5d17f669b9b4b7729ff551d7ce6523aae013eedc
Scott <scottdouglas238@gmail.com>
2020-10-15 14:02:08 -0700

    user data

--- client/src/components/display/box.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-
-function Box({ children }) {
-    return <div className="box">{children}</div>;
-} export default Box;
--- config/middleware/isAuthenticated.js
+++
@@ -1,7 +0,0 @@
-// This is middleware for restricting routes a user is not allowed to visit if not logged in
-module.exports = function (req, res, next) {
-  if (req.user) return next();
-  return res.status(401).json({
-    message: "Not Authenticated!",
-  });
-};
