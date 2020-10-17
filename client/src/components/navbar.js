354682c21b68083e0f35d5187d6dda2c3f3b7c33
mbone1 <m.bonnev10@gmail.com>
2020-10-17 16:13:14 -0700

    formatting for landing page

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,20 +1,35 @@
 import React from "react";
 import LessonPlanButton from "./newLessonPlanbutton"
+import { useStoreContext } from "../utils/GlobalState";
+import "../css/style.scss"
+
 
 function Navbar() {
+    const [globalState, setGlobalState] = useStoreContext();
+
   return (
     <nav className="navbar bk">
-     
-        <div className="navbar-brand">
-          <span className="navbar-item has-text-white is-size-3 has-text-weight-bold">
+      <div className="navbar-brand">
+        <span className="navbar-item has-text-white is-size-1 has-text-weight-bold">
           L-Plan
-          </span>
-          <span className="navbar-item ">
-        <LessonPlanButton/>
+        </span>
+
+        {/* <span className="navbar-item has-text-white is-size-6 has-text-weight-light">
+            Welcome {globalState.user.firstName}!
+          </span> */}
+        <div className="navbar-start">
+          <span className="navbar-item">
+            <LessonPlanButton />
           </span>
         </div>
+      </div>
+      <div className="navbar-end ml-6">
+        <span className="navbar-item has-text-white ml-6" id="logOut">
+          Log out
+        </span>
+      </div>
     </nav>
-          );
+  );
         }
         export default Navbar;
           
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,26 +4,34 @@ import LessonTile from "../components/lessonTiles/LessonTile"
 import DepartmentTile from "../components/lessonTiles/departmenttile"
 import Navbar from "../components/navbar";
 import API from '../utils/API';
+import { useStoreContext } from "../utils/GlobalState";
+
 
 function TeacherLanding() {
-  
+      const [globalState, setGlobalState] = useStoreContext();
+
   return (
     <>
       <Navbar />
-      <div className="section">
-        <div className="container is-fluid">
-          <div className="row">
-            <h3>Your Lessons:</h3>
-            <LessonTile />
-          </div>
-          <div className="row">
-            <h3>Lessons in your Department:</h3>
-           <DepartmentTile />
-          </div>
+      <div className="column">
+        <span className="has-text-white is-size-4">
+          Welcome <strong className="has-text-white">{globalState.user.firstName}</strong>!
+        </span>
+      </div>
+      {/* <div className="section"> */}
+      <div className="container is-fluid">
+        <div className="row">
+          <h3 className="has-text-white">Your Lessons:</h3>
+          <LessonTile />
+        </div>
+        <div className="row">
+          <h3 className="has-text-white">Lessons in your Department:</h3>
+          <DepartmentTile />
         </div>
       </div>
+      {/* </div> */}
     </>
-  )
+  );
 }
 
 export default TeacherLanding;
