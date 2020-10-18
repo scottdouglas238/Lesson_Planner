15323e5fcb4332bdaa9e6ff6c93db5ce8a333274
mbone1 <m.bonnev10@gmail.com>
2020-10-18 10:27:20 -0700

    view page completed

--- client/src/components/display/display.js
+++ client/src/components/display/display.js
@@ -4,6 +4,7 @@ import Navbar from "../navbar";
 import API from "../../utils/API";
 import Label from "./label"
 import Box from "./box"
+import "../../css/style.scss"
 
 function Display(props) {
   const [lesson, setLesson] = useState({});
@@ -23,101 +24,125 @@ function Display(props) {
     <>
       <Navbar />
       <div className="container">
-        <div className="column">
-          <Label>
-            <h5>Teacher Name:</h5>
-          </Label>
-          <Box>
-            <p>{lesson.teacherName}</p>
-          </Box>
+        <div className="columns">
+          <div className="column">
+            <Label>
+              <h5 className="has-text-white">Lesson Name:</h5>
+            </Label>
+            <Box>
+              <p>{lesson.lessonName}</p>
+            </Box>
+          </div>
+          <div className="column">
+            <Label>
+              <h5 className="has-text-white">Teacher Name:</h5>
+            </Label>
+            <Box>
+              <p>{lesson.teacherName}</p>
+            </Box>
+          </div>
+          <div className="column">
+            <Label>
+              <h5 className="has-text-white">Department:</h5>
+            </Label>
 
-          <Label>
-            <h5>Lesson Name:</h5>
-          </Label>
-          <Box>
-            <p>{lesson.lessonName}</p>
-          </Box>
-          <Label>
-            <h5>Department:</h5>
-          </Label>
+            <Box>
+              <p>{lesson.department}</p>
+            </Box>
+          </div>
+          <div className="column">
+            <Label>
+              <h5 className="has-text-white">Course:</h5>
+            </Label>
+            <Box>
+              <p>{lesson.course}</p>
+            </Box>
+          </div>
+          <div className="column">
+            <Label>
+              <h5 className="has-text-white">Grade Level:</h5>
+            </Label>
+            <Box>
+              <p>{lesson.gradeLevel}</p>
+            </Box>
+          </div>
         </div>
-        <Box>
-          <p>{lesson.department}</p>
-        </Box>
+        {/* ↑first group of items */}
+        <div className="columns">
+          <div className="column">
+            <Label>
+              <h5 className="has-text-white">Concepts:</h5>
+            </Label>
+            <Box>
+              <p>{lesson.concepts}</p>
+            </Box>
+          </div>
+          <div className="column">
+            <Label>
+              <h5 className="has-text-white">Standards:</h5>
+            </Label>
+            <Box>
+              <p>{lesson.standards}</p>
+            </Box>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column">
+            <Label>
+              <h5 className="has-text-white">Skills:</h5>
+            </Label>
+            <Box>
+              <p>{lesson.skills}</p>
+            </Box>
+          </div>
+          <div className="column">
+            <Label>
+              <h5 className="has-text-white">Objectives:</h5>
+            </Label>
+            <Box>
+              <p>{lesson.objectives}</p>
+            </Box>
+          </div>
+        </div>
         <Label>
-          <h5>Course:</h5>
-        </Label>
-        <Box>
-          <p>{lesson.course}</p>
-        </Box>
-        <Label>
-          <h5>Grade Level:</h5>
-        </Label>
-        <Box>
-          <p>{lesson.gradeLevel}</p>
-        </Box>
-        <Label>
-          <h5>Concepts:</h5>
-        </Label>
-        <Box>
-          <p>{lesson.concepts}</p>
-        </Box>
-
-        <Label>
-          <h5>Standards:</h5>
-        </Label>
-        <Box>
-          <p>{lesson.standards}</p>
-        </Box>
-        <Label>
-          <h5>Skills:</h5>
-        </Label>
-        <Box>
-          <p>{lesson.skills}</p>
-        </Box>
-        <Label>
-          <h5>Objectives:</h5>
-        </Label>
-        <Box>
-          <p>{lesson.objectives}</p>
-        </Box>
-        <Label>
-          <h5>Materials:</h5>
+          <h5 className="has-text-white">Materials:</h5>
         </Label>
         <Box>
           <p>{lesson.materials}</p>
         </Box>
         <Label>
-          <h5>Goal:</h5>
+          <h5 className="has-text-white">Goal:</h5>
         </Label>
         <Box>
           <p>{lesson.goal}</p>
         </Box>
         <Label>
-          <h5>Opening Activity:</h5>
+          <h5 className="has-text-white">Opening Activity:</h5>
         </Label>
         <Box>
           <p>{lesson.openingActivity}</p>
         </Box>
         <Label>
-          <h5>Activity:</h5>
+          <h5 className="has-text-white">Activity:</h5>
         </Label>
         <Box>
           <p>{lesson.activity}</p>
         </Box>
         <Label>
-          <h5>Assessment:</h5>
+          <h5 className="has-text-white">Assessment:</h5>
         </Label>
         <Box>
           <p>{lesson.assessment}</p>
         </Box>
         <Label>
-          <h5>Closing Acitivity:</h5>
+          <h5 className="has-text-white">Closing Activity:</h5>
         </Label>
         <Box>
           <p>{lesson.closingActivity}</p>
         </Box>
+      <button className="cardBtn">Go back</button>
       </div>
+      <br></br>
     </>
   );
 }
