d33a47647a311a7a00a6181d2f923edf741e4388
Scott <scottdouglas238@gmail.com>
2020-10-18 09:46:38 -0700

    Log out link functional

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import LessonPlanButton from "./newLessonPlanbutton"
 import { useStoreContext } from "../utils/GlobalState";
 import "../css/style.scss"
-
+import { Link } from "react-router-dom";
 
 function Navbar() {
     const [globalState, setGlobalState] = useStoreContext();
@@ -13,10 +13,6 @@ function Navbar() {
         <span className="navbar-item has-text-white is-size-1 has-text-weight-bold">
           L-Plan
         </span>
-
-        {/* <span className="navbar-item has-text-white is-size-6 has-text-weight-light">
-            Welcome {globalState.user.firstName}!
-          </span> */}
         <div className="navbar-start">
           <span className="navbar-item">
             <LessonPlanButton />
@@ -24,8 +20,8 @@ function Navbar() {
         </div>
       </div>
       <div className="navbar-end ml-6">
-        <span className="navbar-item has-text-white ml-6" id="logOut">
-          Log out
+        <span className="navbar-item logOut ml-6" id="logOut">
+          <Link style={{ color: 'inherit'}} to="/">Log out</Link>
         </span>
       </div>
     </nav>
